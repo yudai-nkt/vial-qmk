42acce82a3ae32da2b00e6349e447ccd83108eaa
Yudai NAKATA <yudai.nkt@gmail.com>
2024-07-21 23:01:07 +0900

    feat: add basic Vial support

---
+++ keyboards/aki27/cocot46plus/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4D, 0x5C, 0xCE, 0x1F, 0xB7, 0x02, 0xDC, 0x8F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
